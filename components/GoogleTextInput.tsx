4d7799e725d5b6bbff47cf1a0d752c7ed7a13376
Muhammad Jahanzeb <i.am.jahanzeb6@gmail.com>
2025-05-18 20:25:07 +0500

    implement map functionality

--- components/GoogleTextInput.tsx
+++ components/GoogleTextInput.tsx
@@ -1,7 +1,7 @@
 import { View, Text } from "react-native";
 import React from "react";
 import { GoogleInputProps } from "@/types/types";
-
+import { GooglePlacesAutocomplete } from "react-native-google-places-autocomplete";
 const GoogleTextInput = ({
   icon,
   initialLocation,
@@ -13,7 +13,38 @@ const GoogleTextInput = ({
     <View
       className={`flex-row items-center justify-center relative z-50 rounded-xl ${containerStyle} mb-5`}
     >
-      <Text>Search</Text>
+      <GooglePlacesAutocomplete
+        fetchDetails={true}
+        placeholder="Where you want to go?"
+        debounce={200}
+        styles={{
+          textInputContainer: {
+            alignItems: "center",
+            justifyContent: "center",
+            borderRadius: 20,
+            marginHorizontal: 20,
+            position: "relative",
+            shadowColor: "#d4d4d4",
+          },
+          textInput: {
+            backgroundColor: textInputBackgroundColor || "white",
+            fontSize: 16,
+            fontWeight: "600",
+            marginTop: 5,
+            width: "100%",
+            borderRadius: 200,
+          },
+          listView: {
+            backgroundColor: textInputBackgroundColor || "white",
+            position: "relative",
+            top: 0,
+            width: "100%",
+            borderRadius: 10,
+            shadowColor: "#d4d4d4",
+            zIndex: 99,
+          },
+        }}
+      />
     </View>
   );
 };
--- store/index.ts
+++ store/index.ts
@@ -1,4 +1,4 @@
-import { LocationStore } from "@/types/types";
+import { DriverStore, LocationStore, MarkerData } from "@/types/types";
 import { create } from "zustand";
 
 export const useLocationStore = create<LocationStore>((set) => ({
@@ -38,4 +38,12 @@ export const useLocationStore = create<LocationStore>((set) => ({
       destinationAddress: address,
     }));
   },
+}));
+export const useDriverStore = create<DriverStore>((set) => ({
+  drivers: [] as MarkerData[],
+  selectedDriver: null,
+  setSelectedDriver: (driveId: number) =>
+    set(() => ({ selectedDriver: driveId })),
+  setDrivers: (drivers: MarkerData[]) => set(() => ({ drivers: drivers })),
+  clearSelectedDriver: () => set(() => ({ selectedDriver: null })),
 }));
